c6d66b9c2bcf90189e8f47b0005c732fefc5e827
sune1998 <sune.bro@ka-net.dk>
2019-11-30 16:42:29 +0100

    hej

--- src/Input.py
+++ src/Input.py
@@ -1,7 +1,15 @@
 import sys
+
 print("hello what is your name")
 
 name = sys.stdin.readline()
 
 print("hello", name)
 
+print("enter your age")
+
+age = int(sys.stdin.readline())
+if age > 18:
+    print("welcome")
+else:
+    print("you're not old enough")
