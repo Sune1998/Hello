e2b71a503d3f947f72c3386ab3c3b85564e59478
sune1998 <sune.bro@ka-net.dk>
2019-11-30 16:28:28 +0100

    hej

---
+++ src/Hello.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+num1 = 3
+num2 = 3
+
+new_list = [1, 2, 3, 5]
+print(new_list)
+
+new_list = [1, 2, 3, 5]
+
+num3 = num1 + num2
+print(num3)
+
+if num3 == 6:
+    print("nice")
+else:
+    print("not nice")
+
+
+
